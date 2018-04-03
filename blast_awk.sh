8cf584795365b3c0f19b165adf5359725bd05e06
Taryn <taryn.athey@gmail.com>
2018-04-03 11:40:27 -0400

    Reverse Complement
    Added a step to reverse complement the sequence

--- blast_awk.sh
+++ blast_awk.sh
@@ -26,13 +26,40 @@ for FILENAME in *.fasta ;
 	#blasts the fasta file
 	blastn -db $DB -query $FILENAME -outfmt 6 > $NAME.blast
 
-	#Creates an array of contig names found in blast output
+	#Creates an array of contig names and blast positions found in blast output
 	INTEREST=($(awk '{ print $1 }' $NAME.blast))
+	START=($(awk '{ print $9 }' $NAME.blast))
+	END=($(awk '{ print $10 }' $NAME.blast))
 
 	#Loops through array and writes the sequences to a new file
-	for i in "${INTEREST[@]}"
+	for (( i=0; i<${#INTEREST[@]}; i++ ));
 	do
-		awk 'BEGIN {RS=">"}/'$i'/ {print ">"$o}' $NAME.fasta >> $NAME.contigs.fasta
+		#Writes contig sequence to a temporary file
+		awk 'BEGIN {RS=">"}/'${INTEREST[$i]}'/ {print ">"$o}' $NAME.fasta > $NAME.temp.fasta$i
+
+		#Grabs length of fasta lines
+		LINE_LEN=$(expr $(head -n2 $NAME.temp.fasta$i | tail -n1 | wc -c) - 1)
+
+		#Checks if the blast mapped to the reverse complement, if yes, reverses the sequence
+		if [ "${START[$i]}" -gt "${END[$i]}" ]
+		then
+			#Reverse complements the contig sequences and writes it to a temporary file
+			awk 'BEGIN{RS=">";FS="\n";a["T"]="A";a["A"]="T";a["C"]="G";a["G"]="C";a["N"]="N"}NR>1{for (i=2;i<=NF;i++) seq=seq""$i;for(i=length(seq);i!=0;i--) {k=substr(seq,i,1);x=x a[k]}; printf ">%s\n%s",$1,x}' $NAME.temp.fasta$i >> $NAME.fasta.REV$i
+
+			#Changes the length of the lines of the temporary file
+			awk 'BEGIN{RS=">";FS="\n"}NR>1{seq="";for (i=2;i<=NF;i++) seq=seq""$i;a[$1]=seq;b[$1]=length(seq)}END{for (i in a) {k=sprintf("%d", (b[i]/'$LINE_LEN')+1); printf ">%s\n",i;for (j=1;j<=int(k);j++) printf "%s\n", substr(a[i],1+(j-1)*'$LINE_LEN','$LINE_LEN')}}' $NAME.fasta.REV$i >> $NAME.fasta.REVLINE$i
+
+			#Replaces the original temp file with the reverse complemented file
+			mv $NAME.fasta.REVLINE$i $NAME.temp.fasta$i
+		fi
 	done
+
+	#Add strain name to contigs
+	cat $NAME.temp.fasta* > $NAME.contigs.fasta
+	sed -i s/\>/\>$NAME\_/g $NAME.contigs.fasta
  fi
 done
+
+#Remove all temporary files
+rm *.temp.fasta*
+rm *.REV*
